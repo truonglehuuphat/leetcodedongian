d829c4eb82f7e170567180ed95f3f065c2590761
PhatTruong <phattruongspkt@gmail.com>
2026-04-14 12:11:25 +0700

    Time: 884 ms (5.01%), Space: 47.6 MB (83.23%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,10 +1,14 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        List<Pair<Integer,Integer>> list = new ArrayList<>();
+        // List<Pair<Integer,Integer>> list = new ArrayList<>();
         List<Integer> arr = new ArrayList<>();
         for(String s: timePoints){
-            int mins = string2MM(s);
-            arr.add(mins);
+            // int mins = string2MM(s);
+            char[] arrStr = s.toCharArray();
+            int hour = (arrStr[0]-'0') * 10 + (arrStr[1]-'0');
+            int minute = (arrStr[3]-'0') * 10 + (arrStr[4]-'0');
+            int totalMin = hour * 60+ minute;
+            arr.add(totalMin);
         }
         Collections.sort(arr);
         int min = 10000;
@@ -50,7 +54,6 @@ class Solution {
         Pair<Integer,Integer> pair= new Pair<>(hour,min);
         return pair;
     }
-
     int string2MM(String hhmm){
         int sum = 0;
         int hour = 0;
@@ -60,37 +63,7 @@ class Solution {
         min = Integer.parseInt(arr[1]);
         return hour*60+ min;
     }
-
     int subtime(int time1, int time2){
         return Math.min(Math.abs(time1 - time2), 24*60 - Math.abs(time1 - time2));
-    }
-    int subtr(Pair<Integer,Integer> p1, Pair<Integer,Integer> p2){
-        int res = 0;
-        int min =0;
-        int count = 0;
-        //  System.out.println(p1.m + " " + p2.m);
-        if(p1.m > p2.m){
-            if(p1.m > p2.m){
-                min = 60 - p1.m + p2.m ;
-                count = 1;
-            } else {
-                min =  p2.m - p1.m;
-            }
-        } else {
-            min = Math.abs(p2.m - p1.m);
-        }
-        int minH = 0;
-        if(Math.abs(p1.H - p2.H)>=12){
-            if(p1.H > p2.H ) {
-                minH = 24 - p1.H + p2.H;
-            }  else {
-                minH = p2.H - p1.H;
-            }
-        } else {
-            minH = Math.abs(p2.H - p1.H);
-        }
-        // System.out.println(minH + " "+ count+ " "+ min);
-        res = (minH - count) * 60 + min;
-        return res;
     }
 }
