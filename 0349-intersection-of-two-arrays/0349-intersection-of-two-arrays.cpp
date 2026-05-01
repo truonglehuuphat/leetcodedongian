e53da4acb8b83d5984f523eaedd0db20559d24bd
PhatTruong <phattruongspkt@gmail.com>
2026-05-01 12:39:54 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>res;
+        set<int>ans;
         int i = 0, j= 0; 
         int n = nums1.size(); int m = nums2.size();
-        for(int k = 0 ; k < n; k++){
-            for(int l = 0 ; l < m ; l++){
-                if(nums1[k] == nums2[l]){
-                    auto it = find(res.begin(), res.end(), nums2[l]);
-                    if (it == res.end()){
-                        res.push_back(nums2[l]);
-                    }
-                    break;
-                }
+        // for(int k = 0 ; k < n; k++){
+        //     for(int l = 0 ; l < m ; l++){
+        //         if(nums1[k] == nums2[l]){
+        //             auto it = find(res.begin(), res.end(), nums2[l]);
+        //             if (it == res.end()){
+        //                 res.push_back(nums2[l]);
+        //             }
+        //             break;
+        //         }
+        //     }
+        // }
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        while(i < n && j < m){
+            if(nums1[i] == nums2[j]){
+                ans.insert(nums2[j]);
+                i++;j++;
+            } else if(nums1[i] < nums2[j]){
+                i++;
+            } else {
+                j++;
             }
         }
-
+        vector<int> res;
+        for(int x: ans){
+            res.push_back(x);
+        }
         return res;
     }
 };
